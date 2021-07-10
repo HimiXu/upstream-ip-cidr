ca97be8823e3b1cbbe3a086e4755715787b62953
razcohen <raz@icohens.com>
2021-07-10 23:17:24 +0300

    Comment fix and small refactor

--- src/main/kotlin/upstream/IpBlocker.kt
+++ src/main/kotlin/upstream/IpBlocker.kt
@@ -14,11 +14,10 @@ class IpBlocker(suspiciousIps: List<String>) {
         if (index >= 0) return false;
         // we found the placement
         val insertionPoint = -(index+1)
-        // if the placement is at the end of the array then we should compare the last element
+        // if the placement is at the beginning of the array then its smaller than every suspiciousIp
         if (insertionPoint == 0) return true
-        // otherwise we should compare to the value currently in placement
-        val ipRange = suspiciousIps[insertionPoint-1]
-        return numericIp !in ipRange
+        // otherwise we should check if the ip is within range of the item in insertionPoint-1
+        return numericIp !in suspiciousIps[insertionPoint-1]
     }
 }
 
