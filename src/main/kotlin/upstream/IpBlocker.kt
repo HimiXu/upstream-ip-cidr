c61d270a27e501456348d7735a9d769b79c9f062
razcohen <raz@icohens.com>
2021-07-10 19:55:21 +0300

    Fixed another bug with last element in array Added some comments to explain algorithm

--- src/main/kotlin/upstream/IpBlocker.kt
+++ src/main/kotlin/upstream/IpBlocker.kt
@@ -1,14 +1,21 @@
 package upstream
 
 class IpBlocker(suspiciousIps: List<String>) {
+
+    // this is a sorted and merged list of the suspicious ip ranges
     private val suspiciousIps: List<IpRange> = suspiciousIps.map { it.toIpRange() }.merge()
 
     fun isAllowed(incomingIp: String): Boolean {
         val numericIp = incomingIp.toNumericIp()
+        // it searches for the placement of the numericIp in the suspiciousIps list
         val index = suspiciousIps.binarySearch { it.to.compareTo(numericIp) }
+        // if we found an index, then it means we have a range starting with that value, it is not allowed
         if (index >= 0) return false;
+        // we found the placement
         val insertionPoint = -(index+1)
-        if (insertionPoint >= suspiciousIps.size) return true;
+        // if the placement is at the end of the array then we should compare the last element
+        if (insertionPoint == suspiciousIps.size) return numericIp !in suspiciousIps[suspiciousIps.size-1]
+        // otherwise we should compare to the value currently in placement
         val ipRange = suspiciousIps[insertionPoint]
         return numericIp !in ipRange
     }
--- src/main/kotlin/upstream/IpRange.kt
+++ src/main/kotlin/upstream/IpRange.kt
@@ -12,17 +12,34 @@ data class IpRange(val from: Long, val to: Long) {
     }
 }
 
+/**
+ * @return IpRange represented in that string
+ */
 fun String.toIpRange(): IpRange {
+
     if(!this.matches(Regex("^([0-9]{1,3}.){3}[0-9]{1,3}($|/([0-9]{1,3}))$")))
         throw IllegalArgumentException("String is not a CIDR notation")
-    val (ip, notation) = this.split("/");
-    val hosts = (2.0).pow(32 - notation.toInt()).toInt()
+
+    val (ip, ones) = this.split("/");
+    // calculate the amount of hosts by cidr notation
+    val hosts = (2.0).pow(32 - ones.toInt()).toInt()
+
     val numericIp = ip.toNumericIp()
-    val from = numericIp - numericIp % hosts;
-    val to = from + hosts-1;
-    return IpRange(from, to)
+
+    // calculate the range of valid ips in the current mask
+    val fromIp = numericIp - numericIp % hosts;
+    val toIp = fromIp + hosts-1;
+
+    return IpRange(fromIp, toIp)
 }
 
+/**
+ * This function takes a list of IpRanges
+ * - sorts the list
+ * - merges any intersecting ranges
+ *
+ * @return sorted merged list of IpRanges
+ */
 fun List<IpRange>.merge(): List<IpRange> {
     val sorted = this.sortedBy { it.from }
     val merged = mutableListOf<IpRange>()
@@ -39,6 +56,9 @@ fun List<IpRange>.merge(): List<IpRange> {
     return merged
 }
 
+/**
+ * @return numeric value of an iPv4 address (base 256 for every segment)
+ */
 fun String.toNumericIp(): Long {
     if(!this.matches(Regex("^([0-9]{1,3}.){3}[0-9]{1,3}$")))
         throw IllegalArgumentException("String is not an iPv4 address")
